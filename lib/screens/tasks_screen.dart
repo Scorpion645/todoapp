2597e86dfbf676e53138bd88ea7ccd2154497cb2
Scorpion645 <scorpion645@gmail.com>
2022-11-28 23:42:58 -0500

    after video 89, initial commit

---
+++ lib/screens/tasks_screen.dart
@@ -0,0 +1,62 @@
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
+class TasksScreen extends StatelessWidget {
+  const TasksScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: Colors.indigo[400],
+        child: Icon(Icons.add),
+      ),
+      backgroundColor: Colors.teal[800],
+      body: Container(
+        padding:
+            const EdgeInsets.only(top: 60, right: 20, left: 20, bottom: 80),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              children: [
+                Icon(
+                  Icons.playlist_add_check,
+                  size: 40,
+                  color: Colors.white,
+                ),
+                SizedBox(
+                  width: 20,
+                ),
+                Text(
+                  'ToDayDo',
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 40,
+                      fontWeight: FontWeight.bold),
+                )
+              ],
+            ),
+            Text(
+              '4 tasks',
+              style: TextStyle(color: Colors.white, fontSize: 18),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Expanded(
+              child: Container(
+                decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
